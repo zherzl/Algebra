cbbc9289e6f572b90d529d8acbed0d9cc7a2e51d
Zlatko <zlatko.herzl@outlook.com>
2019-05-25 11:57:07 +0200

    Flow layout - gumbi foreach

--- Vjezbe-zadaci/LayoutManager_primjer/src/gui/FlowLayoutPrimjer.java
+++ Vjezbe-zadaci/LayoutManager_primjer/src/gui/FlowLayoutPrimjer.java
@@ -1,9 +1,9 @@
 //FlowLayoutPrimjer.java
 package gui;
 
-import java.awt.FlowLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
 
 public class FlowLayoutPrimjer extends JFrame {
 
@@ -17,11 +17,15 @@ public class FlowLayoutPrimjer extends JFrame {
     JButton btn8;
     JButton btn9;
 
+    // Nakon izrade flow layouta, prebaciti na Grid layout. -> prvo bez redaka i kolona, a zatim 3x3
+    // i potom uključiti ikonu za gumbiće
+
     public FlowLayoutPrimjer(String naziv) {
         super(naziv);
 //postavi FlowLayout upravitelj zadataka
         setLayout(new FlowLayout());
         btn1 = new JButton("1");
+        //btn1.setIcon(getImageFromFile("1.png"));
         btn2 = new JButton("2");
         btn3 = new JButton("3");
         btn4 = new JButton("4");
@@ -41,10 +45,33 @@ public class FlowLayoutPrimjer extends JFrame {
         getContentPane().add(btn9);
         setBounds(400, 300, 300, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // for (Component c : this.getContentPane().getComponents()) {
+        //     if (c instanceof JButton) {
+        //         ((JButton) c).setBorder(null);
+        //     }
+        // }
+
         setVisible(true);
     }
 
     public static void main(String[] args) {
         FlowLayoutPrimjer jtp = new FlowLayoutPrimjer("FlowLayout Primjer");
+    }
+
+
+    private Icon getImageFromFile(String naziv) {
+        Image img;
+        
+        try {
+            img = ImageIO.read(new File(naziv));
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        
+        
+        return new ImageIcon(img);
     }
 }
